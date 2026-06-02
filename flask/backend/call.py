19107cab4a4973e2b72cff0436a335189954caa5
vongockhang06 <vongockhang2006@gmail.com>
2026-06-02 21:32:19 +0800

    adding .log to .gitignore

--- flask/backend/call.py
+++ flask/backend/call.py
@@ -1,16 +1,19 @@
 import requests
 import os
 import json
+import logging
 from dotenv import load_dotenv
 from datetime import date,datetime
 #print("Import successfully") #For checking
-
-
 load_dotenv()
-#===============Location of taichung=======================
-
 
 #===============Set up====================
+logging.basicConfig(
+    filename='data.log',
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
 API_KEY=os.getenv("API_KEY")
 #print(API_KEY) #For checking
 url = "https://api.openweathermap.org/data/4.0/onecall/timeline/1day"
@@ -22,8 +25,11 @@ params={
     'lang': 'en'
 }
 #Reading API documentation for more details
-
-response=requests.get(url=url,params=params)
-data=response.json()['data']
-with open('taichung_weather.json',mode='w') as file:
-    json.dump(data,file,indent=4,ensure_ascii=False)
+try:
+    response=requests.get(url=url,params=params)
+    data=response.json()
+    with open('taichung_weather.json',mode='a') as file:
+        json.dump(data,file,indent=4,ensure_ascii=False)
+        logging.info(f'file is successfully written')
+except:
+    logging.error(f'Failed to write file')
