fd12bcc159ce6a42d9bb518a9ff19402e058da58
vongockhang06 <vongockhang2006@gmail.com>
2026-07-28 13:35:35 +0800

    hamming exercise

---
+++ bash/hamming.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+main(){
+    s1="$1"
+    s2="$2"
+    if [[ "$#1" != "$#2" ]]; then
+        echo "strands must be of equal length"
+        exit 1
+    else
+        for
+    fi
+}
+main "$@"
